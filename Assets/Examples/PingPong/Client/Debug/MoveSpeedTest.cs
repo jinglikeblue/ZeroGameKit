f7a472c61f33b312883cfa306d3e6cef38a10417
Jing <122300890@qq.com>
2024-01-04 18:25:23 +0800

    PingPong场景

---
+++ Assets/Examples/PingPong/Client/Debug/MoveSpeedTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSpeedTest : MonoBehaviour
+{
+    public float speedX = 0;
+    int speedVectorX = 1;
+
+    public float speedY = 0;
+    int speedVectorY = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {               
+        var moveDistanceX = Time.deltaTime * speedX * speedVectorX;
+        var moveDistanceY = Time.deltaTime * speedY * speedVectorY;
+        var pos = transform.localPosition;  
+        
+        pos.x += moveDistanceX;
+        if(pos.x > 8.5f)
+        {
+            pos.x = 8.5f;
+            speedVectorX *= -1;
+        }
+        else if(pos.x < -8.5f)
+        {
+            pos.x = -8.5f;
+            speedVectorX *= -1;
+        }
+
+        pos.z += moveDistanceY;
+        if(pos.z > 11.5f)
+        {
+            pos.z = 11.5f;
+            speedVectorY *= -1;
+        }
+        else if(pos.z < -11.5f)
+        {
+            pos.z = -11.5f;
+            speedVectorY *= -1;
+        }
+
+
+        transform.localPosition = pos;
+    }
+}
--- Assets/Examples/PingPong/Client/GameCore/Consts/Define.cs
+++ Assets/Examples/PingPong/Client/GameCore/Consts/Define.cs
@@ -10,13 +10,28 @@ namespace PingPong
     public static class Define
     {
         /// <summary>
+        /// 场地的大小
+        /// </summary>
+        public static readonly Vector2 WORLD_SIZE = new Vector2(20, 30);
+
+        /// <summary>
+        /// 球的半径大小
+        /// </summary>
+        public static readonly Number BALL_SIZE = new Number(5, 10);
+
+        /// <summary>
         /// 球的初始位置
         /// </summary>
         public static readonly Vector2 BALL_INITIAL_POSITION = new Vector2(0, 0);
 
         /// <summary>
-        /// 玩家的初始位置
+        /// 玩家的大小
         /// </summary>
-        public static readonly Vector2[] PLAYER_INITIAL_POSITION = new Vector2[] { new Vector2(0, 0), new Vector2(0, 0) };
+        public static readonly Vector2 PLAYER_SIZE = new Vector2(3, 1);
+
+        /// <summary>
+        /// 玩家的初始位置[Player0,Player1]
+        /// </summary>
+        public static readonly Vector2[] PLAYER_INITIAL_POSITION = new Vector2[] { new Vector2(0, -13), new Vector2(0, 13) };
     }
 }
