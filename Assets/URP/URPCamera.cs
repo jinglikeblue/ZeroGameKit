0779a0656ec35714b59acd7e7cd060449b054f3f
Jing <122300890@qq.com>
2024-01-12 16:56:56 +0800

    URP相机层级管理（2019不支持多个Base相机，所以这里用代码处理）
    TODO：
    - 升级ZeroGameKit的Odin。现在的版本升级到2021会有BUG
    - Reporter日志工具优化。横竖屏切换的时候，让其自动刷新重构。

---
+++ Assets/URP/URPCamera.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class URPCamera : MonoBehaviour
+{
+    Camera _camera;
+
+    public Camera Camera
+    {
+        get
+        {
+            return _camera;
+        }
+    }
+
+    [Header("渲染层级(值越大越靠上)")]
+    public int priority = 0;
+
+    bool _lastCameraEnable;
+
+    int _lastPriority = 0;
+
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+        _lastPriority = priority;
+        Debug.Log($"Awake");
+    }
+
+    private void FixedUpdate()
+    {
+        if (_camera.enabled != _lastCameraEnable)
+        {
+            _lastCameraEnable = _camera.enabled;
+            OnCameraEnableChanged();
+            return;
+        }
+
+        if (_lastPriority != priority)
+        {
+            _lastPriority = priority;
+            URPCameraManager.UpdateCameraPriority(this);
+        }
+    }
+
+    private void OnEnable()
+    {
+        _lastCameraEnable = _camera.enabled;
+        //Debug.Log($"OnEnable {_camera.enabled}");
+        URPCameraManager.UpdateCamera(this);
+    }
+
+    private void OnDisable()
+    {
+        //Debug.Log($"OnDisable {_camera.enabled}");
+        URPCameraManager.UpdateCamera(this);
+    }
+
+    void OnCameraEnableChanged()
+    {
+        //Debug.Log($"Camera Enable Changed {_camera.enabled}");
+        URPCameraManager.UpdateCamera(this);
+    }
+}
---
+++ Assets/URP/URPCameraManager.cs
@@ -0,0 +1,81 @@
+﻿using Jing;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+
+/// <summary>
+/// 管理器
+/// </summary>
+public static class URPCameraManager
+{
+    static HashSet<URPCamera> _urpCameraSet = new HashSet<URPCamera>();
+
+    public static void UpdateCamera(URPCamera urpCamera)
+    {
+        Debug.Log($"[URP Camera] {urpCamera.gameObject.name} use:{urpCamera.Camera.isActiveAndEnabled}");
+
+        //筛选出要使用的相机集合
+        if (urpCamera.Camera.isActiveAndEnabled)
+        {
+            if (false == _urpCameraSet.Contains(urpCamera))
+            {
+                _urpCameraSet.Add(urpCamera);
+                RefreshCameraSettings();
+            }
+        }
+        else
+        {
+            if (_urpCameraSet.Contains(urpCamera))
+            {
+                _urpCameraSet.Remove(urpCamera);
+                RefreshCameraSettings();
+            }
+        }
+
+        //对当前集合中的相机进行排序
+    }
+
+    public static void UpdateCameraPriority(URPCamera urpCamera)
+    {
+        if (urpCamera.Camera.isActiveAndEnabled && _urpCameraSet.Contains(urpCamera))
+        {
+            RefreshCameraSettings();
+        }
+    }
+
+    /// <summary>
+    /// 当集合改变时执行
+    /// </summary>
+    static void RefreshCameraSettings()
+    {
+        Debug.Log($"[URP Camera] 相机集合改变");
+
+        var list = _urpCameraSet.ToList();
+        list.Sort((x, y) => { return x.priority < y.priority ? -1 : 1; });
+
+        Camera baseCamera = null;
+        for (var i = 0; i < list.Count; i++)
+        {
+            var urpCamera = list[i];
+
+            Debug.Log($"[URP Camera] 相机:{urpCamera.gameObject.name} 序列:{urpCamera.priority}");
+
+            if (0 == i)
+            {
+                //Base相机
+                baseCamera = urpCamera.Camera;
+                baseCamera.GetUniversalAdditionalCameraData().renderType = CameraRenderType.Base;
+                baseCamera.GetUniversalAdditionalCameraData().cameraStack.Clear();
+            }
+            else
+            {
+                //Overlay相机
+                urpCamera.Camera.GetUniversalAdditionalCameraData().renderType = CameraRenderType.Overlay;
+                baseCamera.GetUniversalAdditionalCameraData().cameraStack.Add(urpCamera.Camera);
+            }
+        }
+    }
+}
