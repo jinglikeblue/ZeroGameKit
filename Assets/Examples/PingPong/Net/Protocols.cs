cf609e40a979ee636edd0c31993d25bcb3a2dc5f
Jing <122300890@qq.com>
2024-01-29 17:58:09 +0800

    Update

---
+++ Assets/Examples/PingPong/Net/Protocols.cs
@@ -0,0 +1,55 @@
+﻿namespace PingPong
+{
+    public class Protocols
+    {
+        #region Client To Server
+
+        /// <summary>
+        /// 加入主机
+        /// </summary>
+        public struct JoinHostRequest
+        {
+
+        }
+
+        /// <summary>
+        /// 游戏准备好了
+        /// </summary>
+        public struct GameReadyRequest
+        {
+
+        }
+
+        /// <summary>
+        /// 玩家输入
+        /// </summary>
+        public struct InputRequest
+        {
+
+        }
+
+        #endregion
+
+        #region Server To Client
+
+        /// <summary>
+        /// 游戏开始
+        /// </summary>
+        public struct GameStartNotify
+        {
+
+        }
+
+        /// <summary>
+        /// 帧输入数据同步
+        /// </summary>
+        public struct FrameInputNotify
+        {
+
+        }
+
+        #endregion
+
+
+    }
+}
